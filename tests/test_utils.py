488132f4e50487bb887e3f1c9e6ab7eb2d534f40
Hamish Gibbs <hamishgibbs@gmail.com>
2021-01-11 09:46:09 +0000

    new utils tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -3,6 +3,63 @@ from pull_fb import utils
 from datetime import datetime
 
 
+@pytest.fixture
+def example_date_config():
+    return {
+        "start_date": datetime(2020, 1, 1),
+        "end_date": datetime(2020, 1, 2),
+        "frequency": 8,
+    }
+
+
+@pytest.fixture(scope="session")
+def local_config_file(tmpdir_factory):
+
+    fn = tmpdir_factory.mktemp("tmp").join(".config")
+
+    with open(fn, 'w') as f:
+
+        f.write('Britain_TileMovement_ID=1671212783027520\nBritain_TileMovement_Origin=2020_03_10_0')
+
+    return fn
+
+
+@pytest.fixture(scope="session")
+def local_config_file_missing_id(tmpdir_factory):
+
+    fn = tmpdir_factory.mktemp("tmp").join(".config")
+
+    with open(fn, 'w') as f:
+
+        f.write('Britain_TileMovement_Origin=2020_03_10_0')
+
+    return fn
+
+
+@pytest.fixture(scope="session")
+def local_config_file_missing_origin(tmpdir_factory):
+
+    fn = tmpdir_factory.mktemp("tmp").join(".config")
+
+    with open(fn, 'w') as f:
+
+        f.write('Britain_TileMovement_ID=1671212783027520')
+
+    return fn
+
+
+@pytest.fixture(scope="session")
+def local_config_file_malformed(tmpdir_factory):
+
+    fn = tmpdir_factory.mktemp("tmp").join(".config")
+
+    with open(fn, 'w') as f:
+
+        f.write('Britain_Colocation_ID=229180671540661Britain_Colocation_Origin=2020_02_11Britain_TilePopulation_ID=881889318900484')
+
+    return fn
+
+
 # Test date_str_to_datetime
 def test_date_str_to_datetime_hours():
 
@@ -31,15 +88,6 @@ def test_date_str_to_datetime_errors():
         utils.date_str_to_datetime(s)
 
 
-@pytest.fixture
-def example_date_config():
-    return {
-        "start_date": datetime(2020, 1, 1),
-        "end_date": datetime(2020, 1, 2),
-        "frequency": 8,
-    }
-
-
 # Test get_file_dates
 def test_get_file_dates_8h(example_date_config):
 
@@ -66,3 +114,66 @@ def test_get_file_dates_type(example_date_config):
     )
 
     assert type(res[0]) is datetime
+
+
+# test get_config
+def test_get_config_remote():
+
+    path = "https://raw.githubusercontent.com/hamishgibbs/pull_facebook_data_for_good/master/.config"
+
+    res = utils.get_config(path)
+
+    assert type(res) is dict
+
+
+def test_get_config_local(local_config_file):
+
+    res = utils.get_config(str(local_config_file))
+
+    assert type(res) is dict
+
+
+def test_get_config_local_raises_malformed(local_config_file_malformed):
+
+    with pytest.raises(Exception):
+
+        utils.get_config(str(local_config_file_malformed))
+
+
+# test get download variables
+def test_get_download_variables_works(local_config_file):
+
+    now = datetime.now()
+
+    res = utils.get_download_variables('TileMovement',
+                                       'Britain',
+                                       now,
+                                       str(local_config_file))
+
+    assert type(res) is dict
+
+    assert res['dataset_id'] == '1671212783027520'
+
+    assert res['start_date'] == datetime(2020, 3, 10, 0)
+
+    assert res['end_date'] == now
+
+
+def test_get_download_variables_missing_id(local_config_file_missing_id):
+
+    with pytest.raises(KeyError):
+
+        utils.get_download_variables('TileMovement',
+                                     'Britain',
+                                     datetime.now(),
+                                     str(local_config_file_missing_id))
+
+
+def test_get_download_variables_missing_origin(local_config_file_missing_origin):
+
+    with pytest.raises(KeyError):
+
+        utils.get_download_variables('TileMovement',
+                                     'Britain',
+                                     datetime.now(),
+                                     str(local_config_file_missing_origin))
